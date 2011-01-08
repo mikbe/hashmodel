c3f8cd3201f28fdc3ad465839a7793e514b1c26d
Mike Bethany <mikbe.tk@gmail.com>
2011-01-08 16:27:13 -0500

    Changed version number

--- lib/hash_model/version.rb
+++ lib/hash_model/version.rb
@@ -1,8 +1,8 @@
 class HashModel
   module VERSION # :nodoc:
     MAJOR  = 0
-    MINOR  = 1
-    TINY   = 2
+    MINOR  = 2
+    TINY   = 0
     PRE    = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
